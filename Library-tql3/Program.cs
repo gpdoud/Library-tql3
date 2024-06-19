061afa8c9ea2c1e0f73b7969ed59ecf822193885
Greg Doud <gpdoud@gmail.com>
2024-06-19 10:19:31 -0400

    Add project files.

---
+++ Library-tql3/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyLibrary;
+
+namespace Library_tql3;
+
+internal class Program {
+
+    static void Main(string[] args) {
+
+        Console.WriteLine(Class1.About);
+
+        var c1 = new Class1();
+
+        Console.WriteLine(c1.About2);
+    }
+}
---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace MyLibrary;
+
+public class Class1 {
+
+    public static string About { get; set; } = "MyClass1";
+
+    public string About2 { get; set; } = "MyClass1 & About 2";
+
+}
